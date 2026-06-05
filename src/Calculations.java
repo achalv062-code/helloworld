e0f701212e18f5e49c467e13c5ee20b3df9f361c
achalv062-code <achalv062@gmail.com>
2026-06-05 11:09:01 +0530

    next simple Java Program

---
+++ src/Calculations.java
@@ -0,0 +1,14 @@
+public class Calculations {
+    public static void main (String[] args){
+        int a=10;
+        int b=30;
+        int c =20;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a+b-c);
+
+
+
+    }
+}
