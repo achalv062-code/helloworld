68a1d9809d87cdfcd29b49c6390e573e26aafec0
achalv062-code <achalv062@gmail.com>
2026-06-05 10:18:25 +0530

    Another Java Program

---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static void main(String[] args){
+        System.out.println(10+20);
+        System.out.println(100-20);
+        System.out.println(10*20);
+        System.out.println(10/20);
+    }
+}
